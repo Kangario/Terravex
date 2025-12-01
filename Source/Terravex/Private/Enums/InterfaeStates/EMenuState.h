5699877fb2c92494e08c78c08f72196d7aa0df3e
Alexander Trushkov <mr.trushkoff2016@mail.ru>
2025-12-01 15:50:17 +0300

    Update Code

--- Source/Terravex/Private/Enums/InterfaeStates/EMenuState.h
+++ Source/Terravex/Private/Enums/InterfaeStates/EMenuState.h
@@ -6,5 +6,7 @@ enum class EMenuState : uint8
 	MainMenu = 0 UMETA(DisplayName = "MainMenu"),
 	SignInMenu = 1 UMETA(DisplayName = "SignInMenu"),
 	SignUpMenu = 2 UMETA(DisplayName = "SignUpMenu"),
-	UserProfileMenu = 3 UMETA(DisplayName = "UserProfileMenu")
+	UserProfileMenu = 3 UMETA(DisplayName = "UserProfileMenu"),
+	ShopMenu = 4 UMETA(DisplayName = "ShopMenu"),
+	BattleGroup = 5 UMETA(DisplayName = "BattleGroup")
 };
---
+++ Source/Terravex/Public/UI/Controllers/BattleGroupController/BattleGroupMenuController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BattleGroupMenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRAVEX_API UBattleGroupMenuController : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
